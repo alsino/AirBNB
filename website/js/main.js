943c0d805635210ca094423fd84884f4dca36461
Lucas Vogel <contact@vogelino.ch>
2015-01-26 17:19:24 +0100

    Added the capacity bar chart

--- website/js/main.js
+++ website/js/main.js
@@ -172,27 +172,23 @@
 			axis: {
 				x: {
 					tick: {
-						count: 25,
-						fit: true,
-						outer: false,
-						format: function (x) { return Math.round(x); }
+						outer: false
 					}
 				},
 				y: {
 					tick: {
-						fit: true,
-						outer: false,
-						format: function (x) { return Math.round(x); }
+						outer: false
 					}
 				}
 			},
 			size: {
-				height: 200
+				height: 220
 			},
 			legend: {
 				show: false
 			},
 			padding: {
+				top: 20,
 				bottom: 20,
 				left: 25
 			},
@@ -203,6 +199,7 @@
 				pattern: ['#ff656a', '#c9c9c9']
 			},
 			bar: {
+				zerobased: true,
 				width: {
 					ratio: 1.1
 				}
@@ -212,29 +209,54 @@
 			}
 		};
 
+		function roundMe(x) {
+			return Math.round(x);
+		}
+
 		var optionsPriceDistribution = $.extend({}, defaultBarchartOptions, true);
 		optionsPriceDistribution.bindto = '#distribution-chart';
 		optionsPriceDistribution.axis.y.label = 'Anzahl Angebote';
 		optionsPriceDistribution.axis.x.label = 'Preis';
+		optionsPriceDistribution.axis.y.tick.format = roundMe;
+		optionsPriceDistribution.axis.x.tick.format = roundMe;
 		optionsPriceDistribution.data.columns = [[optionsPriceDistribution.axis.x.label, 0, 0, 0, 0, 0, 0, 0, 0, 2, 11, 21, 4, 12, 7, 13, 57, 17, 17, 35, 31, 152, 22, 64, 53, 47, 395, 49, 61, 121, 168, 539, 48, 100, 86, 64, 631, 92, 58, 130, 301, 779, 52, 123, 74, 88, 752, 49, 14, 125, 305, 608, 36, 88, 52, 75, 391, 53, 69, 96, 226, 515, 20, 54, 42, 50, 374, 28, 37, 85, 221, 382, 7, 34, 17, 28, 314, 17, 34, 38, 135, 338, 4, 15, 14, 33, 177, 3, 11, 33, 84, 201, 0, 12, 6, 2, 97, 6, 10, 15, 99, 187, 3, 0, 2, 5, 35, 4, 2, 4, 19, 61, 3, 2, 0, 4, 21, 3, 1, 5, 23, 125, 1, 1, 1, 2, 29, 2, 2, 2, 14, 38, 10, 0, 1, 8, 23, 0, 4, 1, 9, 42, 2, 0, 1, 2, 21, 1, 1, 2, 16, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
 		optionsPriceDistribution.regions = [
 			{axis: 'x', start: 8, end: 153, class: 'inserate-exists'}
 		];
 		optionsPriceDistribution.tooltip.contents = function (d) {
-			if (d[0].value !== 0) {
+			if (d[0].value > 1) {
 				return '<div class="custom-tooltip">' +
-					d[0].value + ' Angebote kosten ' + d[0].index + ' €' +
+					'Es gibt ' + d[0].value + ' Angebote, die ' + d[0].index + ' € kosten' +
+				'</div>';
+			}
+			else if (d[0].value === 1) {
+				return '<div class="custom-tooltip">' +
+					'Es gibt nur ein Angebot, das ' + d[0].index + ' € kostet' +
 				'</div>';
 			}
 			else {
 				return '<div class="custom-tooltip">' +
-						'Es gibt keine Angebote, die ' + d[0].index + ' € kosten' +
+						'Es gibt kein Angebot, das ' + d[0].index + ' € kostet' +
 					'</div>';
 			}
 		};
-		optionsPriceDistribution.tooltip.format.title = function (x) { return x + ' €	'; };
 
 		window.c3.generate(optionsPriceDistribution);
+
+		var optionsUsersDistribution = $.extend({}, defaultBarchartOptions, true);
+		optionsUsersDistribution.bindto = '#users-chart';
+		optionsUsersDistribution.axis.y.label = 'Anzahl Nutzer';
+		optionsUsersDistribution.axis.x.label = 'Durchschnitt an Schlafplätze';
+		optionsUsersDistribution.axis.x.tick.centered = true;
+		optionsUsersDistribution.data.columns = [[optionsUsersDistribution.axis.x.label, 0,749,4657,1397,1779,386,241,46,40,5,10,5,1,1,1,4]];
+		optionsUsersDistribution.padding.left = 35;
+		optionsUsersDistribution.tooltip.contents = function (d) {
+			return '<div class="custom-tooltip">' +
+				d[0].value + ' Nutzer können durchschnittlich ' + d[0].index + ' Schlafplätze anbieten' +
+			'</div>';
+		};
+
+		window.c3.generate(optionsUsersDistribution);
 	}
 
 })( jQuery, window );
